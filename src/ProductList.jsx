a41b76da3ee01535483eb1c829566a0047b3480d
Passa97 <141761823+Passa97@users.noreply.github.com>
2025-01-16 14:07:33 +0100

    site test

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -28,7 +28,7 @@ function ProductList() {
                 {
                     name: "Snake Plant",
                     image: "https://cdn.pixabay.com/photo/2021/01/22/06/04/snake-plant-5939187_1280.jpg",
-                    description: "Produces oxygen at night, improving air quality.",
+                    description: "Produces oxygen at night improving air quality.",
                     cost: "$15"
                 },
                 {
