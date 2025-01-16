6cfb36d3d295886154dfb60a4382bbcefb31be52
Passa97 <141761823+Passa97@users.noreply.github.com>
2025-01-16 12:34:58 +0100

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,23 +7,25 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-      const { name, image, cost } = action.payload;
-      const existingItem = state.items.find(item => item.name === name);
+      const {name, image, cost} = action.payload;
+      const existingItem = state.items.find(item => item.name === name)
       if (existingItem) {
-        existingItem.quantity++;
+        existingItem.quantity++
       } else {
-        state.items.push({ name, image, cost, quantity: 1 });
+        state.items.push({...action.payload, quantity: 1});
       }
+    
     },
     removeItem: (state, action) => {
       state.items = state.items.filter(item => item.name !== action.payload.name);
     },
     updateQuantity: (state, action) => {
-      const { name, quantity } = action.payload;
-      const itemToUpdate = state.items.find(item => item.name === name);
-      if (itemToUpdate) {
-        itemToUpdate.quantity = quantity;
+      const {name, quantity} = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity = quantity;
       }
+    
     },
   },
 });
